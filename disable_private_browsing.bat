3d5e17942ecb4e7a27cdbcf0b3ec13bd727176f1
Satheesh <satheeshcharon@gmail.com>
2022-01-10 22:29:05 +0530

    Add files via upload

---
+++ disable_private_browsing.bat
@@ -0,0 +1,15 @@
+REG ADD HKLM\SOFTWARE\Policies\Google\Chrome /v IncognitoModeAvailability /t REG_DWORD /d 1 /f
+REG ADD HKLM\SOFTWARE\Policies\Google\Chrome /v BrowserGuestModeEnabled /t REG_DWORD /d 0 /f
+REG ADD HKLM\SOFTWARE\Policies\Google\Chrome /v BrowserAddPersonEnabled /t REG_DWORD /d 0 /f
+
+REG ADD HKLM\SOFTWARE\Policies\BraveSoftware\Brave /v IncognitoModeAvailability /t REG_DWORD /d 1 /f
+REG ADD HKLM\SOFTWARE\Policies\BraveSoftware\Brave /v TorDisabled /t REG_DWORD /d 1 /f
+REG ADD HKLM\SOFTWARE\Policies\BraveSoftware\Brave /v BrowserGuestModeEnabled /t REG_DWORD /d 0 /f
+
+REG ADD HKLM\SOFTWARE\Policies\Mozilla\Firefox /v DisablePrivateBrowsing /t REG_DWORD /d 1 /f
+
+
+REG ADD HKLM\SOFTWARE\Policies\Microsoft\Edge /v BrowserGuestModeEnabled /t REG_DWORD /d 0 /f
+REG ADD HKLM\SOFTWARE\Policies\Microsoft\Edge /v BrowserAddProfileEnabled /t REG_DWORD /d 0 /f
+REG ADD HKLM\SOFTWARE\Policies\Microsoft\Edge /v InPrivateModeAvailability /t REG_DWORD /d 1 /f
+
